cd42dac2ea0c073b0960d06af37a4236b654db8c
omrishe <virvir5@gmail.com>
2025-11-14 14:40:54 +0200

    changed a bit of css

--- frontend/src/components/ToggleSwitch.jsx
+++ frontend/src/components/ToggleSwitch.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../../ToggleButton.css";
+import "../App.css";
 
 export default function ToggleSwitch({ label, onToggle }) {
   const [btnIsOn, setbtnIsOn] = useState(false);
